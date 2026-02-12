1e557ab6e416a77b555fae61e0889ba937e2bccb
Firas Moussa <porizm@hotmail.com>
2026-02-12 17:36:34 +0700

    implement calendar

--- src/Web/ClientApp/src/app/home/calendar/calendar-list/calendar-list.component.ts
+++ src/Web/ClientApp/src/app/home/calendar/calendar-list/calendar-list.component.ts
@@ -1,7 +1,13 @@
-import { Component, OnInit, Renderer2 } from '@angular/core'
-import { CalendarOptions, DateSelectArg, EventClickArg, EventApi } from '@fullcalendar/angular'
+import { Component, OnInit, ViewChild, Renderer2 } from '@angular/core'
+import {
+  FullCalendarComponent,
+  DateSelectArg,
+  EventClickArg,
+  EventApi,
+} from '@fullcalendar/angular'
 import { CalendarClient, GetUserCalendarViewModel } from 'src/app/shared/api.generated.clients'
-import { EventInput } from '@fullcalendar/angular'
+import { EventInput, CalendarOptions } from '@fullcalendar/core'
+import dayGridPlugin from '@fullcalendar/daygrid'
 import * as moment from 'moment'
 import { createEventId } from './event-utils'
 
@@ -11,6 +17,23 @@ import { createEventId } from './event-utils'
   styleUrls: ['./calendar-list.component.scss'],
 })
 export class CalendarListComponent implements OnInit {
+  @ViewChild(FullCalendarComponent) calendarComponent!: FullCalendarComponent
+  monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ]
+
+  slimS: any
   link: string
   startDate: string
   title: string
@@ -23,45 +46,97 @@ export class CalendarListComponent implements OnInit {
   clickedOnEvent = false
   eventsData: GetUserCalendarViewModel[]
   calendarOptions: CalendarOptions = {
+    plugins: [dayGridPlugin],
     headerToolbar: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek',
+      start: 'todaysDate prev,next',
+      center: '',
+      end: 'monthJump',
     },
-    eventTimeFormat: {
-      hour: 'numeric',
-      meridiem: 'short',
+    customButtons: {
+      monthJump: {
+        text: '',
+        click: () => {},
+      },
+      todaysDate: {
+        text: this.getTodaysDateFormatted(),
+      },
     },
+    datesSet: () => {
+      setTimeout(() => {
+        this.insertMonthSelector()
+      })
+    },
+    // eventTimeFormat: {
+    // 	hour: 'numeric',
+    // 	meridiem: 'short',
+    // },
     initialView: 'dayGridMonth',
     timeZone: 'local',
-    initialEvents: this.events,
-    weekends: true,
-    editable: true,
-    selectable: true,
-    selectMirror: true,
-    dayMaxEvents: true,
-    longPressDelay: 200,
-    eventClick: this.handleEventClick.bind(this),
-    eventsSet: this.handleEvents.bind(this),
-    // dayCellDidMount: ({ date, el, view }) => {
-    //   const eventsWithDate = this.events.filter(event => {
-    //     return moment(event.start).toDate() >= date || moment(event.end).toDate() <= date
-    //   });
-
-    //   if (eventsWithDate.length > 0) {
-    //     el.style.backgroundColor = '#f2f2f2';
-    //   }
-
-    // },
-    // eventDidMount : (eventInfo)=>{
-    //   eventInfo.el.parentElement.parentElement.parentElement.parentElement.style.backgroundColor = '#f2f2f2';
-    // },
-    
+    // initialEvents: this.events,
+    //  weekends: true,
+    //  editable: true,
+    //  selectable: true,
+    //  selectMirror: true,
+    //  dayMaxEvents: true,
+    //  longPressDelay: 200,
+    //  eventClick: this.handleEventClick.bind(this),
+    //  eventsSet: this.handleEvents.bind(this),
   }
 
   currentEvents: EventApi[] = []
 
   constructor(private calendar: CalendarClient, private renderer: Renderer2) {}
+
+  insertMonthSelector() {
+    const calendarApi = this.calendarComponent?.getApi()
+    if (!calendarApi) return
+
+    const calendarEl = document.querySelector('.fc')
+    if (!calendarEl) return
+
+    const button = calendarEl.querySelector('.fc-monthJump-button')
+    if (!button) return
+
+    const toolbarCell = button.parentElement
+    if (!toolbarCell) return
+
+    let monthSelector = toolbarCell.querySelector('#month-selector') as HTMLSelectElement
+
+    if (!monthSelector) {
+      monthSelector = document.createElement('select')
+      monthSelector.id = 'month-selector'
+      monthSelector.style = 'padding:5px 10px;outline:0'
+      this.monthNames.forEach((name, index) => {
+        const option = document.createElement('option')
+        option.value = index.toString()
+        option.textContent = name
+        monthSelector.appendChild(option)
+      })
+
+      monthSelector.addEventListener('change', (e: any) => {
+        const selectedMonth = parseInt(e.target.value, 10)
+        const currentDate = calendarApi.getDate()
+        const targetDate = new Date(currentDate.getFullYear(), selectedMonth, 1)
+        calendarApi.gotoDate(targetDate)
+      })
+      toolbarCell.innerHTML = ''
+      toolbarCell.appendChild(monthSelector)
+    }
+    const currentMonth = calendarApi.getDate().getMonth().toString()
+    if (monthSelector.value !== currentMonth) {
+      monthSelector.value = currentMonth
+    }
+  }
+
+  getTodaysDateFormatted() {
+    const today = new Date()
+    const options = {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+    } as any
+    return today.toLocaleDateString('en-GB', options)
+  }
 
   handleCalendarToggle() {
     this.calendarVisible = !this.calendarVisible
@@ -95,7 +170,8 @@ export class CalendarListComponent implements OnInit {
       if (item[clickInfo.event.groupId] === clickInfo.event.extendedProps.publicId) {
         this.title = item.name
         this.duration = item.duration
-        this.startDate = moment.utc(new Date(item.startDate.toString()))
+        this.startDate = moment
+          .utc(new Date(item.startDate.toString()))
           .lang('en')
           .format('dddd, MMMM D')
       }
@@ -118,14 +194,21 @@ export class CalendarListComponent implements OnInit {
           if (item.type == 2) {
             eventName = 'meetingId'
           }
-          var startDate = moment.utc(new Date(item.startDate.toString())).lang('en').format('YYYY-MM-DD HH:mm:ss');
-          var endDate = moment.utc(new Date(item.endDate.toString())).lang('en').format('YYYY-MM-DD HH:mm:ss');
+          const startDate = moment
+            .utc(new Date(item.startDate.toString()))
+            .lang('en')
+            .format('YYYY-MM-DD HH:mm:ss')
+          const endDate = moment
+            .utc(new Date(item.endDate.toString()))
+            .lang('en')
+            .format('YYYY-MM-DD HH:mm:ss')
           this.events.push({
             publicId: item[eventName],
             groupId: eventName,
             title: item.name,
             start: startDate,
             end: endDate,
+            allDay: true,
           })
         })
 
